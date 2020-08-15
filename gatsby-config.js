32db709b7841bca33eb578925341e72b9a6ec11e
haaaru44 <haaaru4400@gmail.com>
2020-08-15 23:30:07 +0900

    画像追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       options: {
         isTSX: true, // defaults to false
         //jsxPragma: `jsx`, // defaults to "React" ??
-        allExtensions: true
+        allExtensions: true,
       },
     },
     {
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/content/images`,
+        path: `${__dirname}src/content/images`,
       },
     },
     `gatsby-transformer-sharp`,
